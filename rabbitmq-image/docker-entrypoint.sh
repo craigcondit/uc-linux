79cf6c4c701f9cd90477879340a6decc1f3a349a
Craig Condit <ccondit@randomcoder.org>
2015-10-30 11:57:09 -0500

    Updated rabbitmq-server

--- rabbitmq-image/docker-entrypoint.sh
+++ rabbitmq-image/docker-entrypoint.sh
@@ -34,7 +34,36 @@ if [ "$1" = 'rabbitmq-server' ]; then
 		set -e
 
 		echo "RabbitMQ started in local mode..."
+		echo
 
+		echo "Creating internal administrative user..."
+		ADMIN_PASSWORD="$(dd if=/dev/urandom bs=104 count=1 2>/dev/null | md5sum | awk '{print $1}')"
+		cat > /root/.rabbitmqadmin.conf <<EOF
+[default]
+hostname = localhost
+port = 15672
+username = dockeradmin
+password = ${ADMIN_PASSWORD}
+declare_vhost = /
+vhost = /
+EOF
+		chmod 400 /root/.rabbitmqadmin.conf
+		rabbitmqctl add_user dockeradmin "${ADMIN_PASSWORD}"
+		rabbitmqctl set_user_tags dockeradmin administrator
+		rabbitmqctl set_permissions -p / dockeradmin ".*" ".*" ".*"
+
+		echo "Setting cluster name..."
+		rabbitmqctl set_cluster_name rabbit@docker
+
+		echo "Removeing guest user..."
+		rabbitmqctl delete_user guest
+	
+		echo "Setting up admin user (delete this in init.d if you don't want it)..."
+		rabbitmqctl add_user admin admin
+		rabbitmqctl set_user_tags admin administrator
+		rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+	
+		echo "Running init.d scripts..."
 		for f in /docker-entrypoint-init.d/* ; do
 			case "$f" in
 				*.sh)  echo "$0: running $f" ; . "$f" ;;
@@ -45,8 +74,8 @@ if [ "$1" = 'rabbitmq-server' ]; then
 
 		echo "Stopping RabbitMQ..."
 		rabbitmqctl stop
-		killall epmd || true
-		sleep 5
+		killall -9 epmd || true
+		sleep 1
 
 		echo "RabbitMQ configured and ready for use."
 	fi
